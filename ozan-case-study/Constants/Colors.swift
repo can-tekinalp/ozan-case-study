e6cdebb595575192f2f975e2b9cf294ee2c5bd1e
CanTekinalp <can.tekinalp3@gmail.com>
2022-03-02 12:14:28 +0300

    add colors

---
+++ ozan-case-study/Constants/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  ozan-case-study
+//
+//  Created by Developer on 2.03.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let bleachedSilk = UIColor(hex: "#f3f3f3")!
+    static let duskOrange = UIColor(hex: "#fe4c42")!
+    static let lightishGreen = UIColor(hex: "#71dd66")!
+}
